10e0b8d61769a95e7095b37eb0366058f5127b9e
Dimitrios Semitsoglou-Tsiapos <dsemitso@cern.ch>
2015-07-01 16:25:12 +0100

    manage: new method testing

--- invenio_checker/manage.py
+++ invenio_checker/manage.py
@@ -19,6 +19,7 @@
 
 """Manage checker module."""
 
+import inspect
 from functools import wraps
 
 from .common import ALL
@@ -28,9 +29,33 @@ from .rules import Rules
 from .models import CheckerRule
 from invenio.base.factory import create_app
 from invenio.ext.script import Manager, change_command_name
-from invenio.modules.checker.errors import PluginMissing
-from invenio.modules.workflows.models import BibWorkflowObject
+from .errors import PluginMissing
 
+################################################################################
+from invenio.ext.sqlalchemy import db
+from .models import CheckerRule
+from sqlalchemy.exc import IntegrityError
+new_rule = CheckerRule(
+    name='enum',
+    plugin_module='invenio_checker',
+    plugin_file='enum',
+    option_holdingpen=True,
+    option_consider_deleted_records=False,
+    filter_pattern=None,
+    filter_field=None,
+    filter_limit=None,
+)
+try:
+    old=CheckerRule.query.one()
+    db.session.delete(old)
+except:
+    pass
+try:
+    db.session.add(new_rule)
+    db.session.commit()
+except IntegrityError:
+    pass
+################################################################################
 
 manager = Manager(usage=__doc__)
 rules_dec = manager.option('--rules', '-r', default=ALL,
@@ -110,19 +135,28 @@ def run(rules, user_recids, queue, tickets, upload):
         'queue': queue,
         'upload': upload
     }
-    json_rulesets = rules.by_json_ruleset(user_recids)
-    for rule_jsons, recids in json_rulesets.items():
-        data = {
-            'rule_jsons': rule_jsons,
-            'recids': recids,
-            'common': common
-        }
-        obj = BibWorkflowObject.create_object()
-        extra_data = obj.get_extra_data()
-        extra_data.update(data)
-        obj.set_extra_data(extra_data)
-        obj.save()
-        obj.start_workflow("base_bundle", delayed=False)
+
+    def getfile(modspec):
+        """Resolve a modspec into the corresponding python file.
+
+        :param modspec: dot-notated module specifier
+        """
+        path = inspect.getfile(plugin_files[modspec])
+        if path.endswith('.pyc'):
+            path = path[:-1]
+        return path
+
+    for rule in rules:
+        from .tasks import run_test
+        filepath = getfile(rule.pluginspec)
+
+        # celery
+        # z = run_test.delay(filepath, e='2cool')
+
+        # celery sync
+        z = run_test(filepath=filepath)
+
+    # return bool(z)
 
 
 @rules_dec
